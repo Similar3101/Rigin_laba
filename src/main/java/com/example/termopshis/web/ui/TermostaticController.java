d4890500cf6900b9092ae17b6f2c3079137191a8
Александр <alekspomortseff@yandex.ru>
2025-05-22 21:10:22 +0800

    modified:   .gitattributes 	modified:   .gitignore 	modified:   .mvn/wrapper/maven-wrapper.properties 	modified:   pom.xml 	modified:   src/main/java/com/example/termopshis/TermoPshisApplication.java 	renamed:    src/main/java/com/example/termopshis/model/formule.java -> src/main/java/com/example/termopshis/model/Formule.java 	modified:   src/main/java/com/example/termopshis/web/ui/TermostaticController.java 	modified:   src/main/resources/application.properties 	modified:   src/main/resources/templates/result.html 	modified:   src/main/resources/templates/termostatic.html 	modified:   src/test/java/com/example/termopshis/TermoPshisApplicationTests.java

--- src/main/java/com/example/termopshis/web/ui/TermostaticController.java
+++ src/main/java/com/example/termopshis/web/ui/TermostaticController.java
@@ -1,39 +1,65 @@
 package com.example.termopshis.web.ui;
 
 
-import com.example.termopshis.model.formule;
+import com.example.termopshis.model.Formule;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
+//@RestController = Controller + ResponseBody
+@Slf4j
+@RequiredArgsConstructor
 public class TermostaticController {
+
+    private final Formule formule;
+
+//    @Autowired
+//    public TermostaticController(Formule formule) {
+//        this.formule = formule;
+//    }
+    @Bean
 
 
     @GetMapping("/termostatic")
     public String showTermo() {
+        log.info("Get works");
         return "termostatic";
     }
 
     @PostMapping("/termostatic")
     @ResponseBody
     public Map<String, Object> calculatePressure(
-            @RequestParam double Volume,
             @RequestParam double Temperature,
+            @RequestParam double Period,
+            @RequestParam double MaxVolume,
             @RequestParam double Amount_substans
     ) {
-        formule formule = new formule();
+//        Formule formule = new Formule();
+        log.info("Post works");
 
-        formule.setValume(Volume);
-        formule.setAmount_substans(Amount_substans);
         formule.setTempeture(Temperature);
+        formule.setAmount_substans(Amount_substans);
 
-        double Pressure = formule.mend_Clap_press(Volume);
+        // Список для хранения значений давления
+        List<Double> pressures = new ArrayList<>();
+
+        for (double volume = Period; volume <= MaxVolume; volume += Period) {
+            formule.setValume(volume);
+            double pressure = formule.mend_Clap_press(volume);
+            pressures.add(pressure);
+        }
 
         Map<String, Object> response = new HashMap<>();
-        response.put("Pressure", Pressure);
+        response.put("Pressures", pressures);
         return response;
     }
 }
